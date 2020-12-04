4ac05e5f63325a58bd988a023cf0c6a41f610b4e
ZhangYikaii <zykhelloha@gmail.com>
2020-12-04 20:56:42 +0800

    Update at 2020-12-4 21:17:52

---
+++ run/proto_conv.sh
@@ -0,0 +1,18 @@
+python ../main.py \
+    --max_epoch 200 \
+    --gpu 12 \
+    --model_class ProtoNet \
+    --distance l2 \
+    --backbone_class ConvNet \
+    --dataset MiniImageNet \
+    --way 5 --eval_way 5 \
+    --shot 1 --eval_shot 1 \
+    --query 15 --eval_query 15 \
+    --balance 1 \
+    --temperature 64 \
+    --temperature2 16 \
+    --lr 0.0001 --lr_mul 10 --lr_scheduler step \
+    --step_size 20 \
+    --gamma 0.5 \
+    --eval_interval 1
+    # --init_weights ./saves/initialization/miniimagenet/con-pre.pth
